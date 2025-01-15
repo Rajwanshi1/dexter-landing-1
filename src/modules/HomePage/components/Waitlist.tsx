e6d08c1a6c9f24b70214995fbb363da661e14de2
Sarthak Rajwanshi <sarthak.rajwanshi@gmail.com>
2025-01-15 15:45:32 +0530

    text change

--- src/modules/HomePage/components/Waitlist.tsx
+++ src/modules/HomePage/components/Waitlist.tsx
@@ -84,7 +84,7 @@ const Waitlist = () => {
           />
           <input
             className="border border-black-1000 py-2 px-2 outline-none text-sm text-black-1000 bg-transparent w-full"
-            placeholder="Telegram (optional)"
+            placeholder="Telegram"
             value={tg}
             onChange={(e) => setTg(e.target.value)}
           />
